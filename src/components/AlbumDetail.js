98b6b5c70c986c867e52798f6353b9e1e8b8895b
Julian Khatibi <ajkhatibi@gmail.com>
2017-11-29 14:20:53 -0800

    added scroll ability

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,15 +1,64 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image, image } = album;
+    const { 
+        headerContentStyle, 
+        imageStyle, 
+        imageContainer, 
+        titleText,
+        albumCover 
+    } = styles;
     return (
         <Card>
-            <Text>
-            {props.album.title}
-            </Text>
+            <CardSection>
+                <View style={imageContainer}>
+                    <Image 
+                        style={imageStyle} 
+                        source={{ uri: thumbnail_image }} 
+                        />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text style={titleText}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
+            <CardSection>
+                <Image 
+                source={{ uri: image }}
+                style={albumCover}
+                />
+            </CardSection>
         </Card>
     );
 };
+
+const styles = StyleSheet.create({
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    titleText: {
+        fontSize: 18
+    },
+    imageStyle: {
+        height: 50,
+        width: 50
+    },
+    imageContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 10,
+        marginRight: 10
+    },
+    albumCover: {
+        height: 300,
+        flex: 1,
+        width: null
+    }
+});
 
 export default AlbumDetail;
