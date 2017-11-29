0289edc28dc38abb28c3140858538f6f27b4da3e
Julian Khatibi <ajkhatibi@gmail.com>
2017-11-29 09:54:33 -0800

    added cards and api for card manipulation

---
+++ src/components/AlbumList.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+export default class AlbumList extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            albums: []
+        };
+    }
+
+    componentWillMount() {
+        console.log('Hi this component is working fine');
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({ albums: response.data }))
+            .catch(error => console.log('error data: ', error));
+    }
+
+    renderingAlbumList() {
+        return this.state.albums.map(album => 
+            <AlbumDetail key={album.title} album={album} />
+        );
+    }
+    render() {
+        return (
+            <View>
+            {this.renderingAlbumList()}
+            </View>
+        );
+    }
+}
